65f4a78fbc5e9e5e815d1698d2f345607a88bf55
Shane D Wilson <me@shanedwilson.com>
2018-10-28 07:58:07 -0500

    some styling

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ const displayArtPics = (artPieces) => {
     let domString = "";
     artPieces.forEach(artPiece => {
         domString += `
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${artPiece.photo_link}" alt="Card image cap">
-            <div class="card-body">
-            <h5 class="card-title">${artPiece.title}</h5>
-            <p class="card-text">${artPiece.location}.</p>
+        <div class="card col-md-3 px-0 m-3" style="width: 18rem;">
+            <div class="card-header text-center font-weight-bold">
+                ${artPiece.title}
             </div>
+            <img class="card-img-top" src="${artPiece.photo_link}" alt="Card image cap">
+            <div class="card-body"> 
+            </div>
+            <div class="caption card-footer mt-auto">
+                <p class="card-text">${artPiece.location}.</p>
+            </div> 
         </div>
          `
     })
